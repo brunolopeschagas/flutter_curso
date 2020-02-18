a49e38605acc7669662aee8a6968318dedfd656a
brunolopeschagas <brunolopeschagas@gmail.com>
2020-02-17 21:58:28 -0300

    testando get e update do usuário no banco

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,26 @@ import 'package:http_rest_app/modeos/Usuario.dart';
 import 'package:http_rest_app/ui/ajudante_bd.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   BDadosAjudante db = new BDadosAjudante();
 
-  int usuarioSalvo = await db.insertUsuario(new Usuario("Maria", "aleluia"));
-  print(usuarioSalvo);
-  usuarioSalvo = await db.insertUsuario(new Usuario("Maria", "aleluia"));
-  print(usuarioSalvo);
+  // int usuarioSalvo = await db.insertUsuario(new Usuario("Maria", "aleluia"));
+  // usuarioSalvo = await db.insertUsuario(new Usuario("Maria", "aleluia"));
+  // print(usuarioSalvo);
 
+  //recuperar usuario
+  Usuario user = await db.getUsuario(2);
+  debugPrint("Usuário : ${user.id}");
+  debugPrint("Usuário : ${user.nome}");
+
+  //update do usuario
+  Usuario usuario = new Usuario("Bruno", "123");
+  usuario.id = 2;
+  await db.updateUsuario(usuario);
+  Usuario userUpdated = await db.getUsuario(2);
+  debugPrint("Usuário : ${userUpdated.id}");
+  debugPrint("Usuário : ${userUpdated.nome}");
 
   runApp(new MaterialApp(
     title: "Base de dados SQFlite",
@@ -26,8 +36,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,13 +48,10 @@ class _HomeState extends State<Home> {
         body: Container(
           padding: EdgeInsets.all(10),
           child: Column(
-            children: <Widget>[
-
-            ],
+            children: <Widget>[],
           ),
         ),
       ),
     );
   }
-
 }
